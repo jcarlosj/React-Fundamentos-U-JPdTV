47e2030e7c44b331373306c0d9ca9d8c571ba05e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-08 10:55:10 -0500

    [+] Component > Header (JSX): Insertando expresiones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Header() {
+    /** Propiedades */
+    const siteTitle = 'Tienda Virtual',
+          newElement = <h2 id="site-header" className="site-header site-title">{ siteTitle }</h2>;
 
+    /** */
     return (
-        <h2>Header</h2>
+        newElement
     );
 }
 
