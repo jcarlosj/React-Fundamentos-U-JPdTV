56f3d8a0209f0f54856f4bc3655caf383bbef378
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-08 12:04:10 -0500

    [-] Component > Footer: llaves de la funcion & 'return'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-/** Function Expression */
-const Footer = () => {
-    return (
+/** Function Expression (Arrow Function)
+    No requiere las llaves de la funcion, ni tampoco el uso de la palabra reservada
+    return, para React los paréntesis cuando se declara este tipo de función
+    implicitamente es un return.
+*/
+const Footer = () => (
         <footer>
             <small>React &copy; 2020</small>
         </footer>
     );
-}
 
 export default Footer;
