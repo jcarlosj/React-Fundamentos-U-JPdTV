aae2112541447b33c643f2ddca40f9a2567fe5eb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-08 11:23:28 -0500

    [+] Component > Header (JSX): Pasado como Expresión

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,19 @@ function Header() {
     const siteTitle = 'Tienda Virtual',
           newElement = <h2 id="site-header" className="site-header site-title">{ siteTitle }</h2>;
 
+    function getGreeting( user ) {
+        if( user ) {
+            return <h4>Bienvenido, { user }! estamos para servirte</h4>;
+        }
+        return <h4>Los mejores productos y servicios solo para ti.</h4>;
+    }
+
     /** */
     return (
-        newElement
+        <div>
+            { newElement }
+            { getGreeting() }
+        </div>
     );
 }
 
