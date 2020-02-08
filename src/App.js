04664e98f81858d319221d1adeb6f8262a770ee3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-08 10:44:10 -0500

    [+] Component > Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <Header />
                 <p>Fundamentos básicos de React</p>
             </header>
         </div>
